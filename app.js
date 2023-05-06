957782ccc613f5b1dfff1f1015a591e47cd8ed6e
Ashish Kunal <ashishkunal678@gmail.com>
2023-05-06 16:30:17 +0530

    update

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(session({
-    secret: "bhauchakka.",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
 }));
